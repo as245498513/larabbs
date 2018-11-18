628cf2476cbce28665ba180e92935f7b883be1be
CMJ <meckel_cmj@163.com>
2018-11-18 22:23:21 +0800

    个人页增加Ta的话题和Ta的回复

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -12,7 +12,10 @@ class CategoriesController extends Controller
 
     public function show(Category $category){
     	//读取分类ID关联的话题,并按每20条分页
-    	$topics = Topic::where('category_id',$category->id)->paginate(20);
+    	//$topics = Topic::where('category_id',$category->id)->paginate(20);
+    	//延迟加载解决N+1问题
+    	//$topics->load(['user', 'category']);
+    	$topics = Topic::with('user', 'category')->where('category_id', $category->id)->paginate(20);
 
     	//传参变量话题和分类到模板中
     	return view('topics.index',compact('category','topics'));
