eecaeff4cba7fd8bc4f4372d4c9fc18c66b73369
CMJ <meckel_cmj@163.com>
2018-11-19 22:12:02 +0800

    增加simditor编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
 	<link rel="stylesheet"  href="{{ asset('css/app.css') }}">
+	@yield('styles')
 </head>
 <body>
 	<div id="app" class="{{ route_class() }}-page">
@@ -27,5 +28,7 @@
 
 	<!-- Scripts -->
 	<script src="{{ asset('js/app.js') }}"></script>
+	@yield('scripts')
+	
 </body>
 </html>
