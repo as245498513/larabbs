e467b63c1a3bb2da7ecdd0b5028ec3123d1f5ea7
CMJ <meckel_cmj@163.com>
2018-11-24 23:44:18 +0800

    增加用户注册接口

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -13,7 +13,7 @@ class VerificationCodesController extends Controller
     	$phone = $request->phone;
 
     	if(!app()->environment('production')){
-    		$code = '1234';
+    		$code = '1150';
     	}else{
 	        // 生成4位随机数，左侧补0
 	        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
